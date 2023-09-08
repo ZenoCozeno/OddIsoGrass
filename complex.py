40c64740e8a051559ffc236a4e9b7d23f4604166
Warren Cattani <warren.cattani@gmail.com>
2023-09-08 18:09:54 +0200

    Minimal changes

--- complex.py
+++ complex.py
@@ -6,22 +6,60 @@ import numpy as np
 import math
 
 
-class truncated_complex:
-    def __init__(self, right_resol, left_resol):
-        self.right = right_resol
-        self.left = left_resol
+class complex:
+    def __init__(self, entries):
+        self.entries = entries 
 
-    def partial_tensors(self, second_trunc_cpx,k,n):
-        mixed_prods = [] 
-        mixed_prods.append(self.left.tensor(second_trunc_cpx.left))
-        mixed_prods.append(self.left.tensor(second_trunc_cpx.right))
-        mixed_prods.append(self.right.tensor(second_trunc_cpx.left))
-        mixed_prods.append(self.right.tensor(second_trunc_cpx.right))
-        mixed_cohom = [i.higher_cohom_vanishing(k,n) for i in mixed_prods]
-        min_cohom = np.argmin([i.concentration() for i in mixed_cohom])
-        mixed_cohom[min_cohom].normal_form()
-        return mixed_cohom[min_cohom]
-        
+    def display(self):
+        for i in self.entries:
+            self.entries[i].display()
+            print(f"[{i}]", end= " ")
+            print(" ")
+    
+    def shift(self, index):
+        self.entries = {pos + index: entry for pos, entry in self.entries.items()}
+    
+    def stupid_truncation(self, rel_cutting_point):
+        m = max(self.entries.keys())
+        trunc_dx = complex({pos: entry for pos, entry in self.entries.items() if pos > m-rel_cutting_point})
+        trunc_sx = complex({pos: entry for pos, entry in self.entries.items() if pos <= m-rel_cutting_point})
+        return truncated_complex(trunc_dx, trunc_sx)
+    
+    def tensor(self, second_complex):
+        partial_sum = defaultdict(complex_entry)
+        for i in self.entries.keys():
+            for j in second_complex.entries.keys():
+                ij_summand = self.entries[i].tensor(second_complex.entries[j])
+                partial_sum[i+j] = partial_sum[i+j].sum(ij_summand)
+        tensored = complex(partial_sum)
+        for i in tensored.entries.keys():
+            tensored.entries[i].normal_form()
+        return tensored
+    
+    def concentration(self):
+        nonzero = [i for i in self.entries if len(self.entries[i].entry)!=0]
+        return len(nonzero)
+    
+    def normal_form(self):
+        avoidable=[]
+        for i in self.entries:
+            self.entries[i].normal_form()
+            if all([j.mult == 0 for j in self.entries[i].entry]):
+                avoidable.append(i)
+        for p in avoidable:
+            del self.entries[p]
+                
+    
+    def higher_cohom_vanishing(self,k,n):
+        nonvanish_cohoms = {i: self.entries[i].higher_cohom_vanishing(k,n) for i in self.entries}
+        nonvanish_cohoms.normal_form()
+        return complex(nonvanish_cohoms)
+    
+    def dual(self):
+        dual_cpx={}
+        for i in self.entries:
+            dual_cpx.update({ -i : self.entries[i].dual()})
+        return complex(dual_cpx)
 
 
 class complex_summand:
@@ -94,60 +132,25 @@ class complex_entry:
             dual_entry = dual_entry.sum(complex_entry([i.dual()]))
         return dual_entry
 
-class complex:
-    def __init__(self, entries):
-        self.entries = entries 
+    
+class truncated_complex:
+    def __init__(self, right_resol, left_resol):
+        self.right = right_resol
+        self.left = left_resol
 
-    def display(self):
-        for i in self.entries:
-            self.entries[i].display()
-            print(f"[{i}]", end= " ")
-            print(" ")
-    
-    def shift(self, index):
-        self.entries = {pos + index: entry for pos, entry in self.entries.items()}
-    
-    def stupid_truncation(self, rel_cutting_point):
-        m = max(self.entries.keys())
-        trunc_dx = complex({pos: entry for pos, entry in self.entries.items() if pos > m-rel_cutting_point})
-        trunc_sx = complex({pos: entry for pos, entry in self.entries.items() if pos <= m-rel_cutting_point})
-        return truncated_complex(trunc_dx, trunc_sx)
-    
-    def tensor(self, second_complex):
-        partial_sum = defaultdict(complex_entry)
-        for i in self.entries.keys():
-            for j in second_complex.entries.keys():
-                ij_summand = self.entries[i].tensor(second_complex.entries[j])
-                partial_sum[i+j] = partial_sum[i+j].sum(ij_summand)
-        tensored = complex(partial_sum)
-        for i in tensored.entries.keys():
-            tensored.entries[i].normal_form()
-        return tensored
-    
-    def concentration(self):
-        nonzero = [i for i in self.entries if len(self.entries[i].entry)!=0]
-        return len(nonzero)
-    
-    def normal_form(self):
-        avoidable=[]
-        for i in self.entries:
-            self.entries[i].normal_form()
-            if all([j.mult == 0 for j in self.entries[i].entry]):
-                avoidable.append(i)
-        for p in avoidable:
-            del self.entries[p]
-                
-    
-    def higher_cohom_vanishing(self,k,n):
-        nonvanish_cohoms = {i: self.entries[i].higher_cohom_vanishing(k,n) for i in self.entries}
-        nonvanish_cohoms.normal_form()
-        return complex(nonvanish_cohoms)
-    
-    def dual(self):
-        dual_cpx={}
-        for i in self.entries:
-            dual_cpx.update({ -i : self.entries[i].dual()})
-        return complex(dual_cpx)
+    def partial_tensors(self, second_trunc_cpx,k,n):
+        mixed_prods = [] 
+        mixed_prods.append(self.left.tensor(second_trunc_cpx.left))
+        mixed_prods.append(self.left.tensor(second_trunc_cpx.right))
+        mixed_prods.append(self.right.tensor(second_trunc_cpx.left))
+        mixed_prods.append(self.right.tensor(second_trunc_cpx.right))
+        mixed_cohom = [i.higher_cohom_vanishing(k,n) for i in mixed_prods]
+        min_cohom = np.argmin([i.concentration() for i in mixed_cohom])
+        mixed_cohom[min_cohom].normal_form()
+        return mixed_cohom[min_cohom]
+        
+
+
 
 def staircase(weight, k, n):
     weight =formatting(weight, k)
